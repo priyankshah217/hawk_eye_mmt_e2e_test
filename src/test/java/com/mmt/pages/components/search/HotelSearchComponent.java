9b6f9beb627493e0e5d5019603d0a4754a76e607
bitweft <bitweft@gmail.com>
2020-05-31 01:06:04 +0800

    Add a room while searching hotels

--- src/test/java/com/mmt/pages/components/search/HotelSearchComponent.java
+++ src/test/java/com/mmt/pages/components/search/HotelSearchComponent.java
@@ -7,16 +7,26 @@ import org.openqa.selenium.By;
 public class HotelSearchComponent extends SearchComponent {
     private By searchButton = By.id("hsw_search_button");
     private By travelFor = By.className("travelFor");
+    private By rooms = By.className("roomGuests");
+    private By addRooms = By.className("btnAddRoom");
+    private By applyRooms = By.className("btnApply");
     private By travelPopup = By.className("travelForPopup");
     private By listElements = By.tagName("li");
 
     public SearchResultPage search() {
-        driver.findElement(searchButton).click();
+        findElement(searchButton).click();
         return new HotelSearchResultPage();
     }
 
     public void populateSearchDetails() {
+        addRoom();
         selectTravellingFor();
+    }
+
+    private void addRoom() {
+        findElement(rooms).click();
+        findElement(addRooms).click();
+        findElement(applyRooms).click();
     }
 
     private void selectTravellingFor() {
