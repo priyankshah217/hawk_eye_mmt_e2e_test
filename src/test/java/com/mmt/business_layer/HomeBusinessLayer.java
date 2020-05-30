6e7f29932890f51939ca8037238877fb88b63407
Priyank Shah <priyank.shah@coupa.com>
2020-05-30 22:23:42 +0530

    - relcoate ServiceOption to enums

---
+++ src/test/java/com/mmt/business_layer/HomeBusinessLayer.java
@@ -0,0 +1,18 @@
+package com.mmt.business_layer;
+
+import com.mmt.helpers.ServiceOption;
+import com.mmt.pages.HomePage;
+
+public class HomeBusinessLayer {
+    private HomePage homePage;
+
+    public HomeBusinessLayer() {
+        homePage = new HomePage();
+    }
+
+    public void selectHotel() {
+        homePage
+                .selectService(ServiceOption.HOTELS)
+                .search();
+    }
+}
