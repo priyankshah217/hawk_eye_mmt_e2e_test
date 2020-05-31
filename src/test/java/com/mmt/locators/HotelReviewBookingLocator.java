1685efc05d234f3486a282022eae39fde35934d0
Priyank Shah <priyank.shah@coupa.com>
2020-05-31 14:40:03 +0530

    - Add records for HotelReviewBookingLocator and removed ServiceOptionsHeaderLocator.java

---
+++ src/test/java/com/mmt/locators/HotelReviewBookingLocator.java
@@ -0,0 +1,23 @@
+package com.mmt.locators;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.mmt.annotations.Locators;
+
+@Locators
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record HotelReviewBookingLocator(@JsonProperty("roomName")
+                                        String firstName,
+                                        @JsonProperty("lastName")
+                                        String lastName,
+                                        @JsonProperty("email")
+                                        String email,
+                                        @JsonProperty("mobileNumber")
+                                        String mobileNumber,
+                                        @JsonProperty("specialRequests")
+                                        String specialRequests,
+                                        @JsonProperty("donation")
+                                        String donation,
+                                        @JsonProperty("paymentButton")
+                                        String paymentButton) {
+}
