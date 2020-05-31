df5e6fc41c951429e613f4cbe7182fce452592d4
Priyank Shah <priyank.shah@coupa.com>
2020-05-31 14:45:07 +0530

    - Change implementation of factories

--- src/test/java/com/mmt/ui_layer/factories/ReviewBookingPageFactory.java
+++ src/test/java/com/mmt/ui_layer/factories/ReviewBookingPageFactory.java
@@ -2,16 +2,13 @@ package com.mmt.ui_layer.factories;
 
 import com.mmt.enums.CommonRunVariables;
 import com.mmt.enums.ServiceOption;
+import com.mmt.helpers.PageHelper;
 import com.mmt.helpers.RunHelper;
-import com.mmt.ui_layer.pages.ReviewBookingPage;
-import com.mmt.ui_layer.pages.hotels.HotelReviewBookingPage;
+import com.mmt.ui_layer.pages.base.ReviewBookingPage;
 
 public class ReviewBookingPageFactory {
     public static ReviewBookingPage getInstance() {
         ServiceOption option = (ServiceOption) RunHelper.getRunData(CommonRunVariables.SERVICE_OPTION);
-        return switch (option) {
-            case HOTELS -> new HotelReviewBookingPage();
-            case DEFAULT -> throw new RuntimeException("Not Implemented");
-        };
+        return PageHelper.getPage(option.name());
     }
 }
--- src/test/java/com/mmt/ui_layer/factories/SearchComponentFactory.java
+++ src/test/java/com/mmt/ui_layer/factories/SearchComponentFactory.java
@@ -1,15 +1,11 @@
 package com.mmt.ui_layer.factories;
 
 import com.mmt.enums.ServiceOption;
-import com.mmt.ui_layer.components.search.FlightSearchComponent;
-import com.mmt.ui_layer.components.search.HotelSearchComponent;
+import com.mmt.helpers.PageHelper;
 import com.mmt.ui_layer.components.search.SearchComponent;
 
 public class SearchComponentFactory {
     public static SearchComponent getInstance(ServiceOption option) {
-        return switch (option) {
-            case HOTELS -> new HotelSearchComponent();
-            case DEFAULT -> new FlightSearchComponent();
-        };
+        return PageHelper.getSearchComponent(option.name());
     }
 }
